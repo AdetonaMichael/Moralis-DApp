67938951b7dfa85d3bd75c393cb9686d9e4a619e
AdetonaMichael <powerofelectron616@gmail.com>
2022-03-23 09:47:10 +0100

    project is 80% complete as the login is not yet stable and No NFT has been fetched yet

--- js/main.js
+++ js/main.js
@@ -60,9 +60,9 @@ getTransactions = async ()=>{
            let content = `
            <tr>
            <td><a href="https://ropsten.etherscan.io/tx/${element.hash}" target="_blank" rel="noopener noreferrer">${element.hash}</a></td>
-           <td>${element.block_number}</td>
-           <td>${element.block_timestamp}</td>
-           <td>${element.from_address}</td>
+           <td><a href="https://rinkeby.etherscan.io/block/${element.block_number}' target="_blank" rel="noopener noreferrer">${element.block_number}</a></td>
+           <td>${millisecondsToTime(Date.parse(new Date()) - Date.parse(element.block_timestamp))}</td>
+           <td>${element.from_address == Moralis.User.current().get('ethAddress') ? 'Outgoing':'Incoming'}</td>
            <td>${((element.gas * element.gas_price)/1e18).toFixed(5)} ETH</td>
            <td>${(element.value/ 1e18).toFixed(5)} ETH</td> 
        </tr>
@@ -71,6 +71,87 @@ getTransactions = async ()=>{
       });
   }
 }
+
+ getBalances = async()=>{
+// get BSC native balance for a given address
+
+const ethbalance = await Moralis.Web3API.account.getNativeBalance({chain:""});
+const ropstenbalance = await Moralis.Web3API.account.getNativeBalance({chain:"ropsten"});
+const rinkebybalance = await Moralis.Web3API.account.getNativeBalance({options:"rinkeby"});
+console.log((ethbalance.balance/1e18).toFixed(5)+ " ETH");
+console.log((ropstenbalance.balance/1e18).toFixed(5)+ " ETH");
+console.log((rinkebybalance.balance/1e18).toFixed(5)+ " ETH");
+
+let content = document.querySelector('#userbalances').innerHTML = `
+<table class="table table-striped">
+<thead>
+   <tr>
+       <th scope = "col">Chain</th>
+       <th scope = "col">Balance</th>
+   </tr>
+</thead>
+<tbody>
+    <tr>
+       <th>Ether</th>
+       <td>${(ethbalance.balance/1e18).toFixed(5)} ETH</td>
+    </tr>
+    <tr>
+       <th>Ether</th>
+       <td>${(ropstenbalance.balance/1e18).toFixed(5)} ETH</td>
+    </tr>
+    <tr>
+       <th>Ether</th>
+       <td>${(rinkebybalance.balance/1e18).toFixed(5)} ETH</td>
+    </tr>
+</tbody>
+<tbody id="theTransactions">
+
+</tbody>
+</table>
+`
+}
+getnfts = async()=>{
+    let nfts =  await Moralis.Web3API.account.getNFTs({chain: "rinkeby"});
+    console.log(nfts);
+    let tableofNFTs = document.querySelector('#tableofnfts');
+    if(nfts.result.length > 0){
+        nfts.result.forEach(n=>{
+          let metadata =  JSON.parse(n.metadata);
+          let content =    `
+            <div class="card col-md-3" style="width: 18rem;">
+    <img src="${fixURL(metadata.image_url)}" height=300 class="card-img-top" alt="nft image">
+    <div class="card-body">
+        <h5 class="card-title">${metadata.name}</h5>
+        <p class="card-text">${metadata.description}</p>
+
+    </div>
+    </div>
+          `
+          tableofNFTs.innerHTML += content;
+        });
+    };
+}
+fixURL = (url)=>{
+    if(url.startsWith("ipfs")){
+        return "https://ipfs.moralis.io:2053/ipfs/"+url.split("ipfs://").slice(-1)
+    }else{
+        return url +"?format=json"
+    }
+}
+// currenttime(ms) - BlockTime(ms) = Time in Millseconds
+millisecondsToTime = (ms)=>{
+    let minutes = Math.floor((ms/(1000*60)));
+    let hours   = Math.floor((ms/(1000*60*60)));
+    let days    = Math.floor((ms/(1000*60*60*24)));
+    if(days < 1){
+        if(hours <1){
+            if(minutes <1){
+                return `less than a minute ago`
+            } else return `${minutes} minutes(s) ago`
+        }else return `${hours} hours(s) ago`
+    }else  return `${days} days(s) ago`
+}
+
 if(document.getElementById("btn-login") != null){
     document.getElementById("btn-login").onclick = login;
 }
@@ -79,6 +160,12 @@ if(document.getElementById("btn-logout") != null){
 }
 if(document.querySelector('#get-transactions-link') != null){
     document.querySelector('#get-transactions-link').onclick = getTransactions;
+}
+if(document.querySelector('#get-balance-link') != null){
+    document.querySelector('#get-balance-link').onclick = getBalances;
+}
+if(document.querySelector("#get-nfts-link") != null){
+    document.querySelector("#get-nfts-link").onclick = getnfts;
 }
 
 
